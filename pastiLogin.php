b56697c3519eb807bfbb90a64045cbab5262edf5
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-10-17 12:55:25 +0800

    new stuff

--- pastiLogin.php
+++ pastiLogin.php
@@ -21,6 +21,7 @@ require_once "../../configs/pastiConfig.php";
 
 $username = $password = $usertype = "";
 $username_err = $password_err = "";
+$check_filled = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST"){
 
@@ -43,7 +44,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     //look for username+password combo in db
     if(empty($username_err) && empty($password_err)){
 
-        $sql = "SELECT username, password, usertype FROM users WHERE username = ?";
+        $sql = "SELECT username, password, usertype, filled FROM users WHERE username = ?";
 
         if($stmt = mysqli_prepare($link, $sql)){
 
@@ -58,7 +59,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 //1 means username exists, not 1 is wrong username
                 if(mysqli_stmt_num_rows($stmt) == 1){
 
-                    mysqli_stmt_bind_result($stmt, $username, $hashed_password, $usertype);
+                    mysqli_stmt_bind_result($stmt, $username, $hashed_password, $usertype, $check_filled);
                     if(mysqli_stmt_fetch($stmt)){
                         if(password_verify($password, $hashed_password)){
 
@@ -69,7 +70,10 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["usertype"] = $usertype;
 
                             if($usertype === 0){
-                                header("location: pastiUserMain.php");
+                                if($check_filled !== "unfilled")
+                                    header("location: pastiUserMain.php");
+                                else
+                                    header("location: pastiNewParent.php");
                             }
                             elseif($usertype === 1){
                                 header("location: pastiTeacherMain.php");
