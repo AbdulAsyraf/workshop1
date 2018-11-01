85b1ece0a91d333c88c17611f9981eb324bd9cfa
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-02 00:28:13 +0800

    test

--- pastiAdminMain.php
+++ pastiAdminMain.php
@@ -25,19 +25,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     <div class="page-header">
         <h1>Hi admin <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
     </div>
-    <?php
-$filter = mysqli_query($link, "SELECT name from users");
-$menu = "";
-
-while($row = mysqli_fetch_array($filter, MYSQLI_ASSOC){
-?>
-
-    <option value = "teacher"><?php echo $row["name"]; ?></option>
-
-<?php
-
-}
-?>
+    
     <p>
         <a href="pastiLogout.php" class="btn btn-danger">Sign Out</a>
     </p>
