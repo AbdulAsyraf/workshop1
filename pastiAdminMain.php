86caff51de28680ddbec5f160690484a2f68af37
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-02 00:31:57 +0800

    asdasda

--- pastiAdminMain.php
+++ pastiAdminMain.php
@@ -11,7 +11,18 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
 }
 
 ?>
+<?php
+$filter = mysqli_query($link, "SELECT username from users");
 
+while($row = mysqli_fetch_array($filter, MYSQLI_ASSOC){
+?>
+
+    <option value = "teacher"><?php echo $row["username"]; ?></option>
+
+<?php
+
+}
+?>
 
 
 <!DOCTYPE html>
@@ -25,18 +36,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     <div class="page-header">
         <h1>Hi admin <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
     </div>
-    <?php
-$filter = mysqli_query($link, "SELECT username from users");
-
-while($row = mysqli_fetch_array($filter, MYSQLI_ASSOC){
-?>
-
-    <option value = "teacher"><?php echo $row["name"]; ?></option>
-
-<?php
-
-}
-?>
+    
     <p>
         <a href="pastiLogout.php" class="btn btn-danger">Sign Out</a>
     </p>
