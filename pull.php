9d38839152593dc4d7669468a0d2d358d8cdf3c9
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-02 00:55:24 +0800

    asdasdasd

--- pull.php
+++ pull.php
@@ -2,9 +2,11 @@
 
     require_once "../../configs/pastiConfig.php";
 
+    echo "1";
     $sql = "SELECT username from users";
+    echo "2";
     $result = mysqli_query($link, $sql);
-
+    echo "3";
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC)
     printf("%s (%s)\n", $row["username"]);
 
