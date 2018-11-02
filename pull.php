70a5361866b6d9ee97b4b937ce040187ae3097ad
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-02 08:24:26 +0800

    asasd

--- pull.php
+++ pull.php
@@ -7,8 +7,8 @@
     echo "2";
     $result = mysqli_query($link, $sql);
     echo "3";
-    $row = mysqli_fetch_array($result, MYSQLI_ASSOC)
-    /*printf("%s (%s)\n", $row["username"]);
+    $row = mysqli_fetch_array($result, MYSQLI_NUM)
+    echo. $row["username"]);
 
     mysqli_free_result($result);
     mysqli_close($link);*/
