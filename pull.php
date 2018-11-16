7df877cda587cac91feb64e949095c9b03235e9f
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-16 17:03:37 +0800

    qweqweqweqwe

--- pull.php
+++ pull.php
@@ -5,17 +5,27 @@
     $sql = "SELECT a.username, a.name1, b.usertype from parentguardian a, users b where a.username = b.username;";
     $result = mysqli_query($link, $sql);
     
-    echo "<table border='1' width='400'>
+    echo "<table border='1' width='800'>
             <tr>
                 <th>Username</th>
                 <th>Name</th>
+                <th>Usertype</th>
             </tr>";
 
     while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
         echo "<tr>";
         echo "<td width='20%'>" . $row["username"] . "</td>";
         echo "<td width='60%'>" . $row["name1"] . "</td>";
-        echo "<td width='20%'>" . $row["usertype"] . "</td>";
+        if ($row["usertype"] == 0){
+            echo "<td width='20%'>Normal User</td>";
+        }
+        else if ($row["usertype"] == 1){
+            echo "<td width='20%'>Teacher</td>";
+        }
+        else if ($row["usertype"] == 2){
+            echo "<td width='20%'>Administrator</td>";
+        }
+        //echo "<td width='20%'>" . $row["usertype"] . "</td>";
         echo "</tr>";
     }
 
