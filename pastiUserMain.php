3a6888b6e7e196459652b12304ea4dde3e0e5a2b
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-22 19:56:34 +0800

    finishing up

--- pastiUserMain.php
+++ pastiUserMain.php
@@ -7,6 +7,10 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     header("location: pastiLogin.php");
     exit;
 }
+    require_once "../../configs/pastiConfig.php";
+
+    $query = "SELECT name, mykid FROM student WHERE username = ".$_SESSION["username"];
+    $result = mysqli_query($link, $query);
 
 ?>
 
@@ -21,6 +25,24 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     <div class="page-header">
         <h1>Hi user <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
     </div>
+    <table border='1' width = '800'>
+        <tr>
+            <th>Name</th>
+            <th>MyKid</th>
+        </tr>
+    
+    <?php while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+        echo "<tr>";
+        echo "<td>" . $row["name"] . "</td>";
+        echo "<td>" . $row["mykid"] . "</td>";
+        echo "</tr>";
+    }?>
+    </table>
+    <?php   mysqli_free_result($result);
+            mysqli_close($link);
+
+    ?>
+
     <p><input type="button" value="Add Parent or Guardian" onclick="location='pastiNewParent.php'" /></p>
     <p><input type="button" value="Add Student" onclick="location='pastiNewStudent.php'" /></p>
     <p><a href="pastiLogout.php" class="btn btn-danger">Sign Out</a></p>
