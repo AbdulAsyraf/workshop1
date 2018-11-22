ef92aa6c1963e8e66bf656c65af06dd938fe2659
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-22 20:05:21 +0800

    finishing up

--- pastiUserMain.php
+++ pastiUserMain.php
@@ -8,6 +8,31 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     exit;
 }   
 
+require_once "../../configs/pastiConfig.php";
+        
+$query = "SELECT name, mykid FROM student WHERE username = '".$_SESSION["username"]. "'";
+$result = mysqli_query($link, $query);
+
+echo "<table border='1' width = '800'>
+        <tr>
+            <th>Name</th>
+            <th>MyKid</th>
+        </tr>";
+
+while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+    echo "<tr>";
+    echo "<td>" . $row["name"] . "</td>";
+    echo "<td>" . $row["mykid"] . "</td>";
+    echo "</tr>";
+}
+
+echo "</table>";
+    
+    
+mysqli_free_result($result);
+mysqli_close($link);
+
+
 ?>
 
 <!DOCTYPE html>
@@ -21,29 +46,6 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     <div class="page-header">
         <h1>Hi user <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
     </div>
-    <table border='1' width = '800'>
-        <tr>
-            <th>Name</th>
-            <th>MyKid</th>
-        </tr>
-    
-    <?php 
-    
-    require_once "../../configs/pastiConfig.php";
-        $query = "SELECT name, mykid FROM student WHERE username = ".$_SESSION["username"];
-        $result = mysqli_query($link, $query);
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-            echo "<tr>";
-            echo "<td>" . $row["name"] . "</td>";
-            echo "<td>" . $row["mykid"] . "</td>";
-            echo "</tr>";
-        }?>
-    </table>
-    <?php   mysqli_free_result($result);
-            mysqli_close($link);
-
-    ?>
-
     <p><input type="button" value="Add Parent or Guardian" onclick="location='pastiNewParent.php'" /></p>
     <p><input type="button" value="Add Student" onclick="location='pastiNewStudent.php'" /></p>
     <p><a href="pastiLogout.php" class="btn btn-danger">Sign Out</a></p>
