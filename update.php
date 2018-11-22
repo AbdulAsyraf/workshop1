4c4480756048da366ff7b63488e1a9430c852fdb
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-23 04:16:43 +0800

    test

---
+++ update.php
@@ -0,0 +1,45 @@
+<?php
+
+    require_once "../../configs/pastiConfig.php";
+
+    $sql1 = "SELECT name FROM student WHERE mykid = '121211045555';";
+    $sql2 = "UPDATE student SET name = 'Updated boi' WHERE mykid = '121211045555';";
+
+    $result = mysqli_query($link, $sql1);
+
+    $row = mysqli_fetch_assoc($result);
+
+    echo $row["name"];
+
+    mysqli_free_result($result);
+
+    mysqli_query($link, $sql2);
+
+    $result = mysqli_query($link, $sql1);
+
+    $row = mysqli_fetch_assoc($result);
+
+    echo $row["name"];
+
+    mysqli_free_result($result);
+    mysqli_close($link);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Stuff</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <p>This is a test page</p>
+</body>
+</html>
