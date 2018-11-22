836498c6b7337494570fb353e8f642bd3274d23e
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-23 02:19:41 +0800

    finishing up

--- pastiChooseEditStudent.php
+++ pastiChooseEditStudent.php
@@ -3,7 +3,6 @@
     session_start();
 
     require_once "../../configs/pastiConfig.php"
-
 ?>
 
 <!DOCTYPE html>
@@ -17,18 +16,23 @@
     <div class="page=header">
         <h1>Please choose which student to edit</h1>
     </div>
+    <form action="pastiEditStudent" method="post">
+        <?php
+            $username = $_SESSION["username"];
+            $query = "SELECT name FROM student WHERE username = '" .$username."'";
+            $result = mysqli_query($link, $query);
 
-    <?php
-        $username = $_SESSION["username"];
-        $query = "SELECT name FROM student WHERE username = '" .$username."'";
-        $result = mysqli_query($link, $query);
+            while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+                echo "<input type='radio' name='choice' value='".$row['name']."'>".$row['name']."<b>";
+            }
 
-        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
-            echo "<input type='radio' name='choice' value='".$row['name']."'>".$row['name'];
-        }
-
-        mysqli_free_result($result);
-        mysqli_close($link);
-    ?>
+            mysqli_free_result($result);
+            mysqli_close($link);
+        ?>
+    </form>
+    <div class="form-group">
+        <input type="submit" class="btn btn-primary" value="Submit">
+        <input type="button" value="Cancel" onclick="location='pastiUserMain.php'">
+    </div>
 </body>
 </html>
