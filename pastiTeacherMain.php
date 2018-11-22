b58b8321f55103ef48cbe1ffbd6da90ef63daad2
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-23 07:36:29 +0800

    finishing up

--- pastiTeacherMain.php
+++ pastiTeacherMain.php
@@ -7,6 +7,8 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     header("location: pastiLogin.php");
     exit;
 }
+
+require_once "../../configs/pastiConfig.php";
 ?>
 
 <!DOCTYPE html>
@@ -20,8 +22,48 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
     <div class="page-header">
         <h1>Hi teacher <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
     </div>
-    <p>
-        <a href="pastiLogout.php" class="btn btn-danger">Sign Out</a>
-    </p>
+    <?php
+
+        $query = "SELECT * FROM student WHERE status = 'Processing';";
+        $result = mysqli_query($link, $query);
+
+        echo "<table border='1'>";
+        echo "<tr>";
+        echo "<th>Name</th>";
+        echo "<th>MyKid</th>";
+        echo "<th>Date of Birth</th>";
+        echo "<th>Birth Certificate Number</th>";
+        echo "<th>Address</th>";
+        echo "<th>Allergy</th>";
+        echo "<th>Illness</th>";
+        echo "</tr>";
+
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            echo "<tr>";
+            echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["mykid"] . "</td>";
+            echo "<td>" . $row["dob"] . "</td>";
+            echo "<td>" . $row["bc"] . "</td>";
+            echo "<td>" . $row["address"] . "</td>";
+            echo "<td>" . $row["allergy"] . "</td>";
+            echo "<td>" . $row["illness"] . "</td>";
+            echo "</tr>";
+        }
+        
+        echo "</table>";
+    ?>
+
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+    <?php
+        while ($row = mysqli_fetch_array($result, MYSQLI_ASSOC)){
+            echo "<input type='radio' name='choice' value='".$row['mykid']."'>".$row['name']."<br>\n";
+        }
+
+        mysqli_free_result($result);
+        mysqli_close($link);
+    ?>
+        <p><input type="submit" value="Approve Student"></p>
+    </form>
+        <p><a href="pastiLogout.php" class="btn btn-danger">Sign Out</a></p>
 </body>
 </html>
