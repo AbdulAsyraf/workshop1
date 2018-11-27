bd66b8cd38e0670715ae46359db16798dd9068fe
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-27 18:47:57 +0800

    123123

--- ageTest.php
+++ ageTest.php
@@ -1,11 +1,11 @@
 <?php
 
-    if($_SERVER["REQUEST_METHOD"]) == "POST"){
-        //$today = new Datetime(date("Y-m-d"));
+    /*if($_SERVER["REQUEST_METHOD"]) == "POST"){
+        $today = new Datetime(date("Y-m-d"));
         $bday = new Datetime(date("Y-m-d", strtotime($_POST["dobinput"])));
-        //$diff = $today->diff($bday);
-        //printf('Your age is %d years old', $diff->Y);
-    }
+        $diff = $today->diff($bday);
+        printf('Your age is %d years old', $diff->Y);
+    }*/
 
 ?>
 
