1ef5fd744366a13d499c6f52d87c7da470b92cbc
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-28 06:38:58 +0800

    type lel

--- pastiAdminMain.php
+++ pastiAdminMain.php
@@ -23,7 +23,7 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || $_SESSION[
         <h1>Hi admin <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>.</h1>
     </div>
     
-    <p><input type="button" value="Approve Students" onclick="location='pastiAdminApproval.php'">
+    <p><input type="button" value="Approve Students" onclick="location='pastiAdminApprove.php'">
     <p><input type="button" value="Remove Students" onclick="location='pastiAdminRemove.php'">
     <p><input type="button" value="Add Teacher" onclick="location='pastiAdminAddTeacher.php'">
     <p><input type="button" value="Remove Teacher" onclick="location='pastiAdminRemoveTeacher.php'">
