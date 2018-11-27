a8a222f958ff21704634741ced5331a929209732
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-11-28 02:50:09 +0800

    elseif lel

--- pastiLogin.php
+++ pastiLogin.php
@@ -72,7 +72,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             if($usertype === 0){
                                 if($check_filled == "unfilled")
                                     header("location: pastiNewparent2.php");
-                                else if($emergency == "No")
+                                elseif($emergency == "No")
                                     header("location: pastiNewemergency.php");
                                 else
                                     header("location: pastiUserMain.php");
