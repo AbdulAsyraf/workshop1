6e0c90f8ccc8b1eaff0ffe353f8ac48ab2c41dce
AbdulAsyraf <abdulasyrafah@gmail.com>
2018-12-06 08:09:38 +0800

    haih

--- pastiFeeReport.php
+++ pastiFeeReport.php
@@ -5,10 +5,8 @@
     $arr_fee = array(0, 0, 0);//[yes, no, total]
     $sql = "SELECT january, february, march, april, may, june, july, august, september, october, november FROM fee;";
     $result = mysqli_query($link, $sql);
-    $rows = mysqli_fetch_array($result);
-    echo $rows[0];
 
-    /*while($rows = mysqli_fetch_array($result, MYSQLI_NUM)){
+    while($rows = mysqli_fetch_array($result, MYSQLI_NUM)){
         $rowLength = count($rows);
         for( $i = 0; $i < $rowLength, $i++){
             if($rows[$i] == "Yes"){
@@ -19,8 +17,12 @@
             }
             $arr_fee[2]++;
         }
-    }*/
+    }
 
     echo "Hi";
     echo $arr_fee[0];
+    echo "Hi";
+    echo $arr_fee[1];
+    echo "Hi";
+    echo $arr_fee[2];
 ?>
